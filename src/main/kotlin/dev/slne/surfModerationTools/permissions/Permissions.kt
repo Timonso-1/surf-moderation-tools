48c1f29683a1ab7c959854e588bdec6648feca2c
MikeyLLP <167801877+MikeyLLP@users.noreply.github.com>
2025-09-13 17:37:15 +0200

    Update src/main/kotlin/dev/slne/surfModerationTools/permissions/Permissions.kt
    Co-authored-by: TheBjoRedCraft  <143264463+TheBjoRedCraft@users.noreply.github.com>

--- src/main/kotlin/dev/slne/surfModerationTools/permissions/Permissions.kt
+++ src/main/kotlin/dev/slne/surfModerationTools/permissions/Permissions.kt
@@ -4,7 +4,7 @@ import dev.slne.surf.surfapi.bukkit.api.permission.PermissionRegistry
 
 object Permissions : PermissionRegistry() {
 
-    private const val PREFIX = "surf-moderation-tools"
+    private const val PREFIX = "surf.moderation.tools"
     private const val COMMAND_PREFIX = "$PREFIX.command"
 
     val COMMAND_ROTATE = create("$COMMAND_PREFIX.rotation")
