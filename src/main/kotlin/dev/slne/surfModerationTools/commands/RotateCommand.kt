77b2ba5bd5d9955203b3732ab36a6d04a7fdb252
MikeyLLP <167801877+MikeyLLP@users.noreply.github.com>
2025-09-13 17:05:18 +0200

    fix: small issues

--- src/main/kotlin/dev/slne/surfModerationTools/commands/RotateCommand.kt
+++ src/main/kotlin/dev/slne/surfModerationTools/commands/RotateCommand.kt
@@ -1,40 +1,39 @@
 package dev.slne.surfModerationTools.commands
 
+import com.github.shynixn.mccoroutine.folia.entityDispatcher
 import com.github.shynixn.mccoroutine.folia.launch
-import com.github.shynixn.mccoroutine.folia.regionDispatcher
-import dev.jorel.commandapi.arguments.PlayerArgument
 import dev.jorel.commandapi.kotlindsl.anyExecutor
 import dev.jorel.commandapi.kotlindsl.commandAPICommand
 import dev.jorel.commandapi.kotlindsl.getValue
+import dev.jorel.commandapi.kotlindsl.playerArgument
 import dev.slne.surf.surfapi.core.api.messages.adventure.sendText
 import dev.slne.surfModerationTools.permissions.Permissions
 import dev.slne.surfModerationTools.plugin
 import kotlinx.coroutines.withContext
-import org.bukkit.OfflinePlayer
+
+import org.bukkit.entity.Player
 
 
 fun rotateCommand() = commandAPICommand("playerRotate") {
-    withArguments(PlayerArgument("player"))
+    playerArgument("player")
     withPermission(Permissions.COMMAND_ROTATE)
 
     anyExecutor { sender, args ->
-        val targetPlayer: OfflinePlayer by args
+        val targetPlayer: Player by args
 
-        val targetLocation = targetPlayer.player?.location
+        val targetLocation = targetPlayer.location
         val randomYaw = (-180..180).random().toFloat()
         val randomPitch = (-90..90).random().toFloat()
 
         plugin.launch {
-            if (targetLocation != null) {
-                withContext(plugin.regionDispatcher(targetLocation)) {
-                    targetLocation.setRotation(randomYaw, randomPitch)
-                    targetPlayer.player?.teleport(targetLocation)
-                }
+            withContext(plugin.entityDispatcher(targetPlayer)) {
+                targetLocation.setRotation(randomYaw, randomPitch)
+                targetPlayer.teleport(targetLocation)
+            }
 
-                sender.sendText {
-                    appendPrefix()
-                    success("Der Spieler wurde rotiert.")
-                }
+            sender.sendText {
+                appendPrefix()
+                success("Der Spieler wurde rotiert.")
             }
         }
     }
