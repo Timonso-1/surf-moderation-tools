c62cab8f82cb06ba83e1efa5a23730617906ad10
MikeyLLP <167801877+MikeyLLP@users.noreply.github.com>
2025-09-17 18:32:40 +0200

    Update src/main/kotlin/dev/slne/surf/moderation/tools/commands/ConfigCommand.kt
    Co-authored-by: TheBjoRedCraft  <143264463+TheBjoRedCraft@users.noreply.github.com>

--- src/main/kotlin/dev/slne/surf/moderation/tools/commands/ConfigCommand.kt
+++ src/main/kotlin/dev/slne/surf/moderation/tools/commands/ConfigCommand.kt
@@ -3,7 +3,7 @@ package dev.slne.surf.moderation.tools.commands
 import dev.jorel.commandapi.kotlindsl.commandAPICommand
 import dev.slne.surf.moderation.tools.utils.ModPermissionRegistry
 
-fun configCommand() = commandAPICommand("config") {
+fun configCommand() = commandAPICommand("surfmodtools") {
     withPermission(ModPermissionRegistry.COMMAND_CONFIG)
     setArtyCooldownCommand()
     reloadConfigCommand()
