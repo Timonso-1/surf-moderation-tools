c169c192927d7e19df794742f7ce2334b0e2fd55
twisti <denizgebauer@web.de>
2026-01-01 20:34:13 +0100

    refactor: update ReloadConfigCommand to use SurfModerationToolConfig for configuration reloading

--- src/main/kotlin/dev/slne/surf/moderation/tools/commands/ReloadConfigCommand.kt
+++ src/main/kotlin/dev/slne/surf/moderation/tools/commands/ReloadConfigCommand.kt
@@ -3,7 +3,7 @@ package dev.slne.surf.moderation.tools.commands
 import dev.jorel.commandapi.CommandAPICommand
 import dev.jorel.commandapi.kotlindsl.anyExecutor
 import dev.jorel.commandapi.kotlindsl.subcommand
-import dev.slne.surf.moderation.tools.plugin
+import dev.slne.surf.moderation.tools.config.SurfModerationToolConfig
 import dev.slne.surf.moderation.tools.utils.PermissionRegistry
 import dev.slne.surf.surfapi.core.api.messages.adventure.sendText
 import kotlin.system.measureTimeMillis
@@ -11,9 +11,8 @@ import kotlin.system.measureTimeMillis
 fun CommandAPICommand.surfModToolsReloadCommand() = subcommand("reload") {
     withPermission(PermissionRegistry.COMMAND_SURF_MOD_TOOLS)
     anyExecutor { sender, _ ->
-
         val ms = measureTimeMillis {
-            plugin.moderationToolConfig.reload()
+            SurfModerationToolConfig.reloadFromFile()
         }
 
         sender.sendText {
