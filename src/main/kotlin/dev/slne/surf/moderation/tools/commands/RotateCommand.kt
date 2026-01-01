3ced13c2ae135926045e9b37ff3d7e998a6160eb
twisti <denizgebauer@web.de>
2026-01-01 20:36:44 +0100

    refactor: update RotateCommand and SetArtyCooldownCommand for coroutine support and improved configuration handling

--- src/main/kotlin/dev/slne/surf/moderation/tools/commands/RotateCommand.kt
+++ src/main/kotlin/dev/slne/surf/moderation/tools/commands/RotateCommand.kt
@@ -1,32 +1,32 @@
 package dev.slne.surf.moderation.tools.commands
 
 import com.github.shynixn.mccoroutine.folia.entityDispatcher
-import com.github.shynixn.mccoroutine.folia.launch
-import dev.jorel.commandapi.kotlindsl.anyExecutor
 import dev.jorel.commandapi.kotlindsl.commandAPICommand
 import dev.jorel.commandapi.kotlindsl.entitySelectorArgumentOnePlayer
 import dev.jorel.commandapi.kotlindsl.getValue
 import dev.slne.surf.moderation.tools.plugin
 import dev.slne.surf.moderation.tools.utils.PermissionRegistry
+import dev.slne.surf.surfapi.bukkit.api.command.executors.anyExecutorSuspend
 import dev.slne.surf.surfapi.core.api.messages.adventure.sendText
+import dev.slne.surf.surfapi.core.api.util.random
+import kotlinx.coroutines.withContext
 import org.bukkit.entity.Player
 
 fun rotateCommand() = commandAPICommand("rotate") {
     entitySelectorArgumentOnePlayer("targetPlayer")
     withPermission(PermissionRegistry.COMMAND_ROTATE)
 
-    anyExecutor { sender, args ->
+    anyExecutorSuspend { sender, args ->
         val targetPlayer: Player by args
 
-        val randomYaw = (-180..180).random().toFloat()
-        val randomPitch = (-90..90).random().toFloat()
+        val randomYaw = random.nextFloat(-180f, 180f)
+        val randomPitch = random.nextFloat(-90f, 90f)
 
-        plugin.launch(plugin.entityDispatcher(targetPlayer)) {
+        withContext(plugin.entityDispatcher(targetPlayer)) {
             targetPlayer.setRotation(randomYaw, randomPitch)
 
             sender.sendText {
                 appendPrefix()
-                success("Der Spieler ")
                 variableValue(targetPlayer.name)
                 success(" wurde erfolgreich rotiert.")
             }
