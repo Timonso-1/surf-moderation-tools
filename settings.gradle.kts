17c63e9f164fd68a9feefc314460d980c74d9b2a
twisti <denizgebauer@web.de>
2026-01-01 19:07:09 +0100

    chore: update Gradle wrapper to version 9.1.0 and surf-api-gradle-plugin to 1.21.11

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,6 @@ buildscript {
         maven("https://repo.slne.dev/repository/maven-public/") { name = "maven-public" }
     }
     dependencies {
-        classpath("dev.slne.surf:surf-api-gradle-plugin:1.21.10+")
+        classpath("dev.slne.surf:surf-api-gradle-plugin:1.21.11+")
     }
 }
